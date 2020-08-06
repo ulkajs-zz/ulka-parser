0aa1941fb8f6d115d591737d65e566cfba66399e
acharyaroshanji <acharyaroshan2357@gmail.com>
2020-08-06 18:46:15 +0545

    basepath and globalOptions for ulka express engine

--- src/engine.js
+++ src/engine.js
@@ -1,10 +1,15 @@
-const parse = require('./parse');
+const path = require('path');
 const fs = require('fs');
+const parse = require('./parse');
 
-const engine = () => {
+const engine = ({ globalOptions }) => {
   return (filePath, options, callback) => {
     const content = fs.readFileSync(filePath, 'utf-8');
-    const parsed = parse(content, options);
+    const parsed = parse(
+      content,
+      { ...globalOptions, ...options },
+      { base: path.dirname(filePath) },
+    );
     return callback(null, parsed);
   };
 };
