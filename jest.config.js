d0c6b22417c56b92b4804341c72bd70c6d0d380f
Roshan Acharya <acharyaroshan2357@gmail.com>
2020-10-09 12:47:26 +0545

    test: add coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   collectCoverage: true,
-};
+  collectCoverageFrom: ["src/compile.js", "src/utils.js", "src/index.js"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
+}
